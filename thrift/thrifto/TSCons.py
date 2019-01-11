01763a4a0c389b367bdbf901e06745e54d93c362
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2019-01-11 14:00:09 +0700

    Add files via upload

--- thrift/thrifto/TSCons.py
+++ thrift/thrifto/TSCons.py
@@ -17,4 +17,20 @@
 # under the License.
 #
 
-__all__ = ['Thrift', 'TSCons']
+from os import path
+from SCons.Builder import Builder
+from six.moves import map
+
+
+def scons_env(env, add=''):
+    opath = path.dirname(path.abspath('$TARGET'))
+    lstr = 'thrift --gen cpp -o ' + opath + ' ' + add + ' $SOURCE'
+    cppbuild = Builder(action=lstr)
+    env.Append(BUILDERS={'ThriftCpp': cppbuild})
+
+
+def gen_cpp(env, dir, file):
+    scons_env(env)
+    suffixes = ['_types.h', '_types.cpp']
+    targets = map(lambda s: 'gen-cpp/' + file + s, suffixes)
+    return env.ThriftCpp(targets, dir + file + '.thrift')
